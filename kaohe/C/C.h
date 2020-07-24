716c8066779c70e5528ee20816fa2a7802135f05
xuxubaobaoyu <1656567238@qq.com>
2020-07-24 14:38:57 +0800

    C分支第一次提交

---
+++ kaohe/C/C.h
@@ -0,0 +1,11 @@
+#ifndef _C_H
+#define _C_H
+
+#ifdef DLL_C
+#define		DLL_IMPORT_EXPORT	__declspec(dllexport)	
+#else
+#define		DLL_IMPORT_EXPORT	__declspec(dllimport)		
+#endif
+DLL_IMPORT_EXPORT	int chen(int a, int b);
+
+#endif
