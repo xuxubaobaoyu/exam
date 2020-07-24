ac45b986d88810c9350d3a6c2054a3c3882fa210
xuxubaobaoyu <1656567238@qq.com>
2020-07-24 17:03:38 +0800

    C分支第二次提交

--- kaohe/C/C.h
+++ kaohe/C/C.h
@@ -2,9 +2,9 @@
 #define _C_H
 
 #ifdef DLL_C
-#define		DLL_IMPORT_EXPORT	__declspec(dllexport)	
+#define		DLL_IMPORT_EXPORT	extern "C" __declspec(dllexport)	
 #else
-#define		DLL_IMPORT_EXPORT	__declspec(dllimport)		
+#define		DLL_IMPORT_EXPORT	extern "C" __declspec(dllimport)		
 #endif
 DLL_IMPORT_EXPORT	int chen(int a, int b);
 
